ffbc0b4b849bfbd9d2a15d22255cc83b28744840
Scott Addie <tobias.addie@gmail.com>
2017-03-31 09:16:51 -0500

    Add in-memory caching to WeatherService

--- ViewComponentsDemo/Services/WeatherService.cs
+++ ViewComponentsDemo/Services/WeatherService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using ViewComponentsDemo.Models;
@@ -14,25 +16,43 @@ namespace ViewComponentsDemo.Services
     public class WeatherService : IWeatherService
     {
         private readonly IConfiguration _configuration;
+        private readonly IMemoryCache _cache;
 
-        public WeatherService(IConfiguration configuration)
+        public WeatherService(IConfiguration configuration,
+                              IMemoryCache cache)
         {
             _configuration = configuration;
+            _cache = cache;
         }
 
         public Task<OpenWeatherMapResponse> GetCurrentWeatherAsync(string city, string stateAbbrev)
         {
-            // Fetch the user secret
-            string apiKey = _configuration.GetValue<string>("OpenWeatherMapApiKey");
-
+            const string WEATHER_CACHE_KEY = "Weather";
             OpenWeatherMapResponse currentWeather = null;
 
-            using (var client = new HttpClient())
+            // Look for cache key
+            if (!_cache.TryGetValue(WEATHER_CACHE_KEY, out currentWeather))
             {
-                var endpointUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city},{stateAbbrev}&appid={apiKey}";
-                var response = client.GetStringAsync(endpointUrl).Result;
+                // Key not in cache, so get data
 
-                currentWeather = JsonConvert.DeserializeObject<OpenWeatherMapResponse>(response);
+                // Fetch the user secret
+                string apiKey = _configuration.GetValue<string>("OpenWeatherMapApiKey");
+
+                using (var client = new HttpClient())
+                {
+                    var endpointUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city},{stateAbbrev}&appid={apiKey}";
+                    var response = client.GetStringAsync(endpointUrl).Result;
+
+                    currentWeather = JsonConvert.DeserializeObject<OpenWeatherMapResponse>(response);
+                }
+
+                // Keep in cache for this duration; reset time if accessed
+                var cacheEntryOptions = new MemoryCacheEntryOptions {
+                    SlidingExpiration = TimeSpan.FromSeconds(10)
+                };
+
+                // Save data in cache
+                _cache.Set(WEATHER_CACHE_KEY, currentWeather, cacheEntryOptions);
             }
 
             return Task.Run(() => { return currentWeather; });
--- ViewComponentsDemo/Startup.cs
+++ ViewComponentsDemo/Startup.cs
@@ -30,6 +30,8 @@ namespace ViewComponentsDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+
             // Add framework services.
             services.AddMvc();
             
