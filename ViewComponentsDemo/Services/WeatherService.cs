a16d9d145e614047a3de88748733783ab5166228
Scott Addie <scott.addie@microsoft.com>
2018-07-08 22:04:32 -0500

    Order using statements

--- ViewComponentsDemo/Services/WeatherService.cs
+++ ViewComponentsDemo/Services/WeatherService.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using ViewComponentsDemo.Mappers;
 using ViewComponentsDemo.Models;
 
