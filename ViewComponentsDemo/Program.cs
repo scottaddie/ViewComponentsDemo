6294a813465dbe3f6a2a974aae6f5033ba6d5015
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2018-08-22 21:11:40 -0500

    Wire up Azure Key Vault (#3)

--- ViewComponentsDemo/Program.cs
+++ ViewComponentsDemo/Program.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace ViewComponentsDemo
 {
@@ -10,6 +15,26 @@ namespace ViewComponentsDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                   .ConfigureAppConfiguration((context, config) =>
+                   {
+                       var keyVaultEndpoint = GetKeyVaultEndpoint();
+
+                       // If the environment variable isn't set, it means we're running locally.
+                       // In that case, use the .NET Core Secret Manager tool to retrieve secrets.
+                       if (!String.IsNullOrEmpty(keyVaultEndpoint))
+                       {
+                           var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                           var keyVaultClient = new KeyVaultClient(
+                               new KeyVaultClient.AuthenticationCallback(
+                                   azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                           config.AddAzureKeyVault(
+                               keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                       }
+                   })
                    .UseStartup<Startup>();
+
+        private static string GetKeyVaultEndpoint() => 
+            Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
     }
 }
