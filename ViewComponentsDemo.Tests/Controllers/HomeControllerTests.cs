78f05108f26ea3fce59d3a75d313e12a61bee972
Scott Addie <scott.addie@microsoft.com>
2018-09-30 21:53:35 -0500

    Add HomeController unit test

---
+++ ViewComponentsDemo.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsDemo.Controllers;
+using Xunit;
+
+namespace ViewComponentsDemo.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void ControllerInvocation_Returns_ViewComponentResult()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.ControllerInvocation();
+
+            // Assert
+            Assert.IsType<ViewComponentResult>(result);
+        }
+    }
+}
